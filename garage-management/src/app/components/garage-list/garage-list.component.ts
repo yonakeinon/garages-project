30ec30d10061f80611c4b0d81f4b91e58b0022b1
yonakeinon <yonakeinon@gmail.com>
2025-02-01 18:42:51 +0200

    Updated project structure and added README documentation

--- garage-management/src/app/components/garage-list/garage-list.component.ts
+++ garage-management/src/app/components/garage-list/garage-list.component.ts
@@ -37,50 +37,42 @@ interface Garage {
 })
 export class GarageListComponent implements OnInit {
   dataSource = new MatTableDataSource<Garage>([]);
-  availableGarages: Garage[] = [];
-  selectedGarages: Garage[] = [];
   displayedColumns: string[] = ['name', 'address', 'city', 'phone'];
   isLoading = false;
+  selectedGarages: Garage[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.loadAllGarages();
+    this.loadGarages();
   }
 
-  loadAllGarages() {
+  loadGarages() {
     this.isLoading = true;
-    
     this.http.get<Garage[]>('http://localhost:3000/api/garages').subscribe({
-      next: (storedGarages) => {
-        console.log('Stored garages:', storedGarages);
-        this.dataSource.data = storedGarages || [];
-        
-        this.http.get<Garage[]>('http://localhost:3000/api/garages/fetch').subscribe({
-          next: (allGarages) => {
-            console.log('All garages:', allGarages);
-            if (!allGarages || allGarages.length === 0) {
-              console.warn('No garages received from fetch endpoint');
-              this.availableGarages = [];
-              this.isLoading = false;
-              return;
-            }
-            
-            const existingIds = new Set(storedGarages.map(g => g.mispar_mosah));
-            this.availableGarages = allGarages.filter(g => !existingIds.has(g.mispar_mosah));
-            console.log('Available garages for multi-select:', this.availableGarages);
-            this.isLoading = false;
-          },
-          error: (error) => {
-            console.error('Error fetching available garages:', error);
-            this.availableGarages = [];
-            this.isLoading = false;
-          }
-        });
+      next: (garages) => {
+        if (garages.length === 0) {
+          this.fetchGarages();
+        } else {
+          this.dataSource.data = garages;
+          this.isLoading = false;
+        }
       },
       error: (error) => {
-        console.error('Error loading stored garages:', error);
-        this.dataSource.data = [];
+        console.error('Error loading garages:', error);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  fetchGarages() {
+    this.http.get<Garage[]>('http://localhost:3000/api/garages/fetch').subscribe({
+      next: (garages) => {
+        this.dataSource.data = garages;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching garages:', error);
         this.isLoading = false;
       }
     });
@@ -97,7 +89,7 @@ export class GarageListComponent implements OnInit {
     import('rxjs').then(({ forkJoin }) => {
       forkJoin(addRequests).subscribe({
         next: () => {
-          this.loadAllGarages();
+          this.loadGarages();
           this.selectedGarages = [];
         },
         error: (error) => {
